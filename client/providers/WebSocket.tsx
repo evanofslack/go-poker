572a8bb68f974ef2606c3d0fe4daaaedc07e2f10
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-03-24 23:50:15 -0600

    create websocket provider

---
+++ client/providers/WebSocket.tsx
@@ -0,0 +1,15 @@
+import { createContext, ReactChild } from "react";
+
+export const isBrowser = typeof window !== "undefined";
+const socket = isBrowser ? new WebSocket("ws://127.0.0.1:8080/ws") : null;
+
+export const SocketContext = createContext(socket);
+
+type SocketProviderProps = {
+    children: ReactChild
+}
+
+export function SocketProvider(props: SocketProviderProps) {
+    return <SocketContext.Provider value={socket}>{props.children}</SocketContext.Provider>
+
+}
