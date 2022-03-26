2dde729fa54de8e5eed3573c90dc43b5e27b987c
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-03-25 23:42:28 -0600

    add take seat action

--- server/game.go
+++ server/game.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/alexclewontin/riverboat"
 	"github.com/google/uuid"
 )
 
@@ -14,9 +15,13 @@ type event struct {
 	Params map[string]any `json:"params"`
 }
 
+// inbound (client) actions
 const actionSendMessage action = "send-message"
-const actionNewMessage action = "new-message"
 const actionNewPlayer action = "new-player"
+const actionTakeSeat action = "take-seat"
+
+// outbound (server) actions
+const actionNewMessage action = "new-message"
 
 const gameAdminName string = "system"
 
@@ -26,6 +31,8 @@ func processEvents(c *Client, e event) {
 		handleSendMessage(c, e.Params["username"].(string), e.Params["message"].(string))
 	case actionNewPlayer:
 		handleNewPlayer(c, e.Params["username"].(string))
+	case actionTakeSeat:
+		handleTakeSeat(c, uint(e.Params["buyIn"].(float64))) // convert JSON/JS float to int
 	default:
 		fmt.Println("Unexpected Action")
 	}
@@ -42,12 +49,30 @@ func createNewMessageEvent(username string, message string) event {
 			"id":        uuid.New().String(),
 			"message":   message,
 			"username":  username,
-			"timestamp": time.Now(),
+			"timestamp": currentTime(),
 		},
 	}
+}
+
+func currentTime() string {
+	return fmt.Sprintf("%d:%d", time.Now().Hour(), time.Now().Minute())
 }
 
 func handleNewPlayer(c *Client, username string) {
 	c.username = username
 	c.hub.broadcast <- createNewMessageEvent(gameAdminName, fmt.Sprintf("%s has joined", username))
+}
+
+func handleTakeSeat(c *Client, buyIn uint) {
+	c.seatID = c.game.AddPlayer()
+	err := riverboat.BuyIn(c.game, c.seatID, buyIn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = riverboat.ToggleReady(c.game, c.seatID, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	godView := c.game.GenerateOmniView()
+	fmt.Printf("%+v\n", godView)
 }
