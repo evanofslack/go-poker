9b74ebd4a55721e9719d95b4371d1bc345b65057
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-03-26 12:26:20 -0600

    add game types

--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -1,26 +1,14 @@
 import Chat from "./Chat";
-import { useSocket } from "../hooks/useSocket";
+import Seat from "./Seat";
 
 export default function Game() {
-    const socket = useSocket();
-
-    function handleClick() {
-        socket?.send(
-            JSON.stringify({
-                action: "take-seat",
-                params: {
-                    buyIn: 1000,
-                },
-            })
-        );
-    }
-
     return (
         <div className="flex h-screen flex-row items-center justify-center">
             <div className="m-24 flex h-1/2 w-3/5 items-center justify-center rounded-3xl bg-green-600">
                 <div>
                     Table
-                    <button onClick={handleClick}>Sit Down</button>
+                    <Seat />
+                    <Seat />
                 </div>
             </div>
             <div className="absolute left-0 bottom-0">
--- client/interfaces/index.ts
+++ client/interfaces/index.ts
@@ -13,3 +13,47 @@ export type Event = {
     action: string;
     params: any;
 };
+
+export type Card = string;
+
+export type Player = {
+    username: string;
+    seatID: number;
+    totalBuyIn: number;
+    stack: number;
+    ready: boolean;
+    in: boolean;
+    called: boolean;
+    left: boolean;
+    bet: number;
+    totalBet: number;
+    cards: Card[];
+};
+
+export type Game = {
+    dealer: Player;
+    action: Player;
+    utg: Player;
+    sb: Player;
+    bb: Player;
+    communityCards: Card[];
+    stage: number;
+    betting: boolean;
+    config: Config;
+    players: Player[];
+};
+
+export type Config = {
+    maxBuyIn: number;
+    bb: number;
+    sb: number;
+};
+
+export type Pot = {
+    topShare: number;
+    amount: number;
+    eligiblePlayers: Player[];
+    winningPlayers: Player[];
+    winningHand: Card[];
+    winningScore: number;
+};
