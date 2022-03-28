6b8b7d274c866b6c0358945699e3ebf7c01784dc
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-03-28 00:57:31 -0400

    added startGame and newPlayer actions

--- client/actions/actions.ts
+++ client/actions/actions.ts
@@ -10,13 +10,34 @@ export function sendMessage(socket: WebSocket, username: string, message: string
     );
 }
 
-export function takeSeat(socket: WebSocket, username: string, buyIn: number) {
+export function takeSeat(socket: WebSocket, username: string, position: number, buyIn: number) {
     socket.send(
         JSON.stringify({
             action: "take-seat",
             params: {
                 username: username,
+                position: position,
                 buyIn: buyIn,
+            },
+        })
+    );
+}
+
+export function startGame(socket: WebSocket) {
+    socket.send(
+        JSON.stringify({
+            action: "start-game",
+            params: {},
+        })
+    );
+}
+
+export function newPlayer(socket: WebSocket, username: string) {
+    socket?.send(
+        JSON.stringify({
+            action: "new-player",
+            params: {
+                username: username,
             },
         })
     );
