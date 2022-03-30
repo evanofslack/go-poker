b333d16599bf62267696d01cd5d85c858f5c87b8
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-03-30 02:20:11 -0400

    implement community cards

---
+++ client/components/CommunityCards.tsx
@@ -0,0 +1,27 @@
+import { useContext, useState, useEffect } from "react";
+import { AppContext } from "../providers/AppStore";
+
+export default function CommunityCards() {
+    const { appState, dispatch } = useContext(AppContext);
+
+    useEffect(() => {
+        console.log(appState.game?.communityCards);
+    });
+
+    if (appState.game?.communityCards) {
+        return (
+            <div className="mt-6 mb-4 flex flex-row p-2 text-2xl text-black">
+                {appState.game?.communityCards.map((c, i) => (
+                    <p key={i}>
+                        {c == "2\u0000" ? (
+                            <p className="mx-2 px-2 py-6">{null}</p>
+                        ) : (
+                            <p className="mx-2 border bg-white px-2 py-6">{c}</p>
+                        )}
+                    </p>
+                ))}
+            </div>
+        );
+    }
+    return null;
+}
