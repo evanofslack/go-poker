4f29ce12837b1147c495f6e1784385825a977ae6
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-04-02 23:29:51 -0400

    add input logic

--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -1,7 +1,8 @@
 import { useState, useContext, useEffect } from "react";
 import { AppContext } from "../providers/AppStore";
-import { playerCall } from "../actions/actions";
+import { playerCall, playerCheck, playerFold, playerRaise } from "../actions/actions";
 import { useSocket } from "../hooks/useSocket";
+import InputButton from "./InputButton";
 
 export default function Input() {
     const socket = useSocket();
@@ -11,20 +12,45 @@ export default function Input() {
             playerCall(socket);
         }
     };
+    const handleRaise = (amount: number) => {
+        if (socket) {
+            playerRaise(socket, amount);
+        }
+    };
+    const handleCheck = () => {
+        if (socket) {
+            playerCheck(socket);
+        }
+    };
+    const handleFold = () => {
+        if (socket) {
+            playerFold(socket);
+        }
+    };
 
-    if (!appState.game || appState.game.players.length === 0) return null;
+    if (!appState.game || appState.game.betting == false) return null;
 
     const action = appState.clientID === appState.game.players[appState.game.action].id;
 
+    const player = appState.game.players[appState.game.action];
+    const playerBets = appState.game.players.map((player) => player.bet);
+    const maxBet = Math.max(...playerBets);
+
+    const canCheck = player.bet >= maxBet;
+    const canCall = maxBet - player.bet === 0;
+    const callAmount = maxBet - player.bet < player.stack ? maxBet - player.bet : player.stack;
+
     if (action) {
         return (
-            <div className="bg-gray-600 p-4 text-white">
-                <div className="flex flex-col bg-gray-800 p-4">
-                    <button onClick={handleCall}>Call</button>
-                    <button>Check</button>
-                    <button>Raise</button>
-                    <button>Fold</button>
-                </div>
+            <div className="flex flex-row p-6">
+                <InputButton
+                    action={handleCall}
+                    title={canCall ? "call" : "call (" + callAmount + ")"}
+                    disabled={canCall}
+                />
+                <InputButton action={() => handleRaise(1000)} title={"raise"} disabled={false} />
+                <InputButton action={handleCheck} title={"check"} disabled={!canCheck} />
+                <InputButton action={handleFold} title={"fold"} disabled={false} />
             </div>
         );
     }
