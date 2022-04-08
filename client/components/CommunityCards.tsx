060662a255fd6d32addc00dc21242301239bcc8d
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-04-08 01:22:45 -0400

    set placeholder cards

--- client/components/CommunityCards.tsx
+++ client/components/CommunityCards.tsx
@@ -5,12 +5,18 @@ import Card from "./Card";
 export default function CommunityCards() {
     const { appState, dispatch } = useContext(AppContext);
 
+    const cards = [0, 1, 2, 3, 4];
+
     if (appState.game?.communityCards) {
         return (
             <div className="mb-4 flex flex-row p-2 text-2xl text-black">
-                {appState.game?.communityCards.map((c, i) => (
+                {cards.map((num, i) => (
                     <div key={i} className="m-0.5">
-                        <Card card={c} width={100} height={130} />
+                        {appState.game?.communityCards[num] ? (
+                            <Card card={appState.game.communityCards[num]} placeholder={false} />
+                        ) : (
+                            <Card card="placeholder" placeholder={true} />
+                        )}
                     </div>
                 ))}
             </div>
