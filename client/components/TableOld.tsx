19f47d0b2c9bc8b4b2812889efed4d9c8c700aa3
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-04-18 22:45:57 -0400

    create new table with grid

--- client/components/TableOld.tsx
+++ client/components/TableOld.tsx
@@ -1,5 +1,6 @@
+import CommunityCards from "./CommunityCards";
+import Pot from "./Pot";
 import Seat from "./Seat";
-import Felt from "./Felt";
 import { Game as GameType, Player } from "../interfaces";
 import { AppContext } from "../providers/AppStore";
 import { sendLog, dealGame } from "../actions/actions";
@@ -57,7 +58,7 @@ function getRevealedPlayers(game: GameType) {
     return revealedPlayers;
 }
 
-export default function Table({ players, setPlayers }: tableProps) {
+export default function TableOld({ players, setPlayers }: tableProps) {
     const socket = useSocket();
     const { appState, dispatch } = useContext(AppContext);
     const game = appState.game;
@@ -102,57 +103,23 @@ export default function Table({ players, setPlayers }: tableProps) {
     }, [game?.pots]);
 
     return (
-        <div className="mt-2 grid h-3/4 w-full max-w-screen-xl grid-cols-5 grid-rows-5 sm:mt-28 sm:h-3/5">
-            <div></div>
-            {/* <div className=" border-2 border border-emerald-800"></div> */}
-            <Seat
-                key={3}
-                player={players[3]}
-                id={4}
-                reveal={players[3] ? revealedPlayers.includes(players[3]) : false}
-            />
-            <div></div>
-            <Seat
-                key={4}
-                player={players[4]}
-                id={5}
-                reveal={players[4] ? revealedPlayers.includes(players[4]) : false}
-            />
-            <div></div>
-            <div></div>
-            <div className="col-span-3 row-span-3 flex items-center justify-center">
-                <Felt />
+        <div className="mx-24  flex h-screen justify-center">
+            <div className="relative mt-36 flex h-1/2 w-5/6 max-w-[800px] flex-col items-center justify-center rounded-full border-8 border border-teal-900 bg-emerald-700">
+                <div className="flex w-full flex-col items-center justify-center">
+                    <Pot />
+                    <div className="mt-8 mb-20 flex w-full items-center justify-center">
+                        <CommunityCards />
+                    </div>
+                </div>
+                {players.map((player, index) => (
+                    <Seat
+                        key={index}
+                        player={player}
+                        id={index + 1}
+                        reveal={player ? revealedPlayers.includes(player) : false}
+                    />
+                ))}
             </div>
-            <div></div>
-            <Seat
-                key={5}
-                player={players[5]}
-                id={6}
-                reveal={players[5] ? revealedPlayers.includes(players[5]) : false}
-            />
-            <Seat
-                key={0}
-                player={players[0]}
-                id={1}
-                reveal={players[0] ? revealedPlayers.includes(players[0]) : false}
-            />
-            <div></div>
-            <div></div>
-            <div></div>
-            <Seat
-                key={1}
-                player={players[1]}
-                id={2}
-                reveal={players[1] ? revealedPlayers.includes(players[1]) : false}
-            />
-            <div></div>
-            <Seat
-                key={2}
-                player={players[2]}
-                id={3}
-                reveal={players[2] ? revealedPlayers.includes(players[2]) : false}
-            />
-            <div></div>
         </div>
     );
 }
