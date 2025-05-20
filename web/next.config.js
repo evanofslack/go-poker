6ce72c8551278adb4a001fb4d35426edd883a46d
Evan Slack <evan.slack@outlook.com>
2025-05-20 00:07:57 -0400

    static nextjs

--- web/next.config.js
+++ web/next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    output: 'standalone',
+    output: 'export',
     reactStrictMode: true,
     images: {
         domains: [],
-    },
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
-            },
-        ];
+        unoptimized: true,
     },
 };
